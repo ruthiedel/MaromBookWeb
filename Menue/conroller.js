5c6a619e6f4bc2d031e3aeacc0ee473053f306f8
ruthiedel <140281471+ruthiedel@users.noreply.github.com>
2024-10-10 13:24:33 +0300

    add-and-update

--- Menue/conroller.js
+++ Menue/conroller.js
@@ -3,13 +3,12 @@
 
 function onLoad() {
     Gbooks = JSON.parse(localStorage.getItem('books'));
-    if(!Gbooks)
-    {
+    if (!Gbooks) {
         Gbooks = Dbooks;
         localStorage.setItem('books', JSON.stringify(Gbooks));
-    } 
+    }
     renderBooks(Gbooks.slice(CurrentBook, CurrentBook + 5));
-    renderNavigateButtons(Math.ceil(Gbooks.length/5),1);
+    renderNavigateButtons(Math.ceil(Gbooks.length / 5), 1);
 }
 
 
@@ -19,81 +18,84 @@ onLoad()
 
 
 
-function onLoadData()
-{
-   
+function onLoadData() {
+
     let bookDetails = document.getElementById("book-details");
     bookDetails.innerHTML = '<h1>Book Details</h1><p>No book selected yet.</p>';
     Gbooks = Dbooks;
     localStorage.setItem('books', JSON.stringify(Gbooks));
     CurrentBook = 0;
     renderBooks(Gbooks.slice(CurrentBook, CurrentBook + 5));
-    renderNavigateButtons(Math.ceil(Gbooks.length/5),1);
+    renderNavigateButtons(Math.ceil(Gbooks.length / 5), 1);
 }
 
 function onNavBtnClick(i) {
 
-    CurrentBook = (i-1)*5;
-    renderBooks(Gbooks.slice(CurrentBook, CurrentBook+5<Gbooks.length?CurrentBook+5:Gbooks.length));
-    renderNavigateButtons(Math.ceil(Gbooks.length/5),i);
+    CurrentBook = (i - 1) * 5;
+    renderBooks(Gbooks.slice(CurrentBook, CurrentBook + 5 < Gbooks.length ? CurrentBook + 5 : Gbooks.length));
+    renderNavigateButtons(Math.ceil(Gbooks.length / 5), i);
 }
 
 
-function onBookClick(id)
-{
-   const book = Gbooks.find(b=>b.id===id);
-   renderBookDetails(book);
+function onBookClick(id) {
+    const book = Gbooks.find(b => b.id === id);
+    renderBookDetails(book);
 }
-  
 
-function onRangeReduce(id)
-{
-    const book = Gbooks.find(b=>b.id===id);
-    book.rate =book.rate-1;
+
+function onRangeReduce(id) {
+    const book = Gbooks.find(b => b.id === id);
+    book.rate = book.rate - 1;
     localStorage.setItem('books', JSON.stringify(Gbooks));
     document.getElementById(`book-rate`).innerText = book.rate;
 }
 
 
-function onRangeIncrease(id)
-{
-    const book = Gbooks.find(b=>b.id===id);
-    book.rate =book.rate+1;
+function onRangeIncrease(id) {
+    const book = Gbooks.find(b => b.id === id);
+    book.rate = book.rate + 1;
     localStorage.setItem('books', JSON.stringify(Gbooks));
     document.getElementById(`book-rate`).innerText = book.rate;
 }
 
 
-function onBookDelete(id)
-{
-    Gbooks = Gbooks.filter(b=>b.id!==id);
+function onBookDelete(id) {
+    Gbooks = Gbooks.filter(b => b.id !== id);
     localStorage.setItem('books', JSON.stringify(Gbooks));
-    renderBooks(Gbooks.slice(CurrentBook, CurrentBook + 5<Gbooks.length?CurrentBook+5:Gbooks.length));
-    renderNavigateButtons(Math.ceil(Gbooks.length/5),1);
+    renderBooks(Gbooks.slice(CurrentBook, CurrentBook + 5 < Gbooks.length ? CurrentBook + 5 : Gbooks.length));
+    renderNavigateButtons(Math.ceil(Gbooks.length / 5), 1);
 }
 
- function onNextClick()
- {
-    if(CurrentBook+5<Gbooks.length)
-    {
-        
-        CurrentBook = CurrentBook+5;
-        renderBooks(Gbooks.slice(CurrentBook, CurrentBook + 5<Gbooks.length?CurrentBook+5:Gbooks.length));
-        renderNavigateButtons(Math.ceil(Gbooks.length/5),Math.floor((CurrentBook+5)/5));
+function onNextClick() {
+    if (CurrentBook + 5 < Gbooks.length) {
+
+        CurrentBook = CurrentBook + 5;
+        renderBooks(Gbooks.slice(CurrentBook, CurrentBook + 5 < Gbooks.length ? CurrentBook + 5 : Gbooks.length));
+        renderNavigateButtons(Math.ceil(Gbooks.length / 5), Math.floor((CurrentBook + 5) / 5));
 
     }
-    
- }
 
- function onPrevClick()
- {
-    if(CurrentBook>=5)
-    {
-        
-        CurrentBook = CurrentBook-5;
-        renderBooks(Gbooks.slice(CurrentBook, CurrentBook + 5<Gbooks.length?CurrentBook+5:Gbooks.length));
-        renderNavigateButtons(Math.ceil(Gbooks.length/5),Math.floor((CurrentBook+5)/5));
+}
+
+function onPrevClick() {
+    if (CurrentBook >= 5) {
+
+        CurrentBook = CurrentBook - 5;
+        renderBooks(Gbooks.slice(CurrentBook, CurrentBook + 5 < Gbooks.length ? CurrentBook + 5 : Gbooks.length));
+        renderNavigateButtons(Math.ceil(Gbooks.length / 5), Math.floor((CurrentBook + 5) / 5));
 
     }
-    
- }
+
+}
+
+
+function onNewBookClick() {
+   openAddSideBar('add');
+}
+
+
+function onBookUpdateClick(id) {
+    const book = Gbooks.find(b => b.id === id);
+    openAddSideBar('update', book);
+
+}
--- Menue/view.js
+++ Menue/view.js
@@ -11,7 +11,7 @@ function renderBooks(books) {
             <td>$${book.price.toFixed(2)}</td>
             <td>
                 <button onclick="onBookClick(${book.id})">Read</button>
-                <button>Update</button>
+                <button onclick="onBookUpdateClick(${book.id})">Update</button>
                 <button class="delete" onclick="onBookDelete(${book.id})">🗑️</button>
             </td>
         `;
@@ -20,28 +20,24 @@ function renderBooks(books) {
 }
 
 
-function renderNavigateButtons(num,selectedPage)
-{
+function renderNavigateButtons(num, selectedPage) {
     const pageButtons = document.getElementById("nav-btns");
     pageButtons.innerHTML = '';
     const btns = document.createElement("div");
     btns.innerHTML = '';
-    for(let i=0;i<num;i++)
-    {
-        if(i+1 == selectedPage)
-        {
-            btns.innerHTML += `<button class = "nav-btn active-btn" onclick="onNavBtnClick(${i+1})">${i+1}</button>`;
+    for (let i = 0; i < num; i++) {
+        if (i + 1 == selectedPage) {
+            btns.innerHTML += `<button class = "nav-btn active-btn" onclick="onNavBtnClick(${i + 1})">${i + 1}</button>`;
         }
-        else{
-            btns.innerHTML += `<button class = "nav-btn" onclick="onNavBtnClick(${i+1})">${i+1}</button>`;
+        else {
+            btns.innerHTML += `<button class = "nav-btn" onclick="onNavBtnClick(${i + 1})">${i + 1}</button>`;
         }
     }
     pageButtons.appendChild(btns);
-    
+
 }
 
-function renderBookDetails(book)
-{
+function renderBookDetails(book) {
     let bookDetails = document.getElementById("book-details");
     bookDetails.innerHTML = '';
     bookDetails.innerHTML = `
@@ -55,4 +51,66 @@ function renderBookDetails(book)
                     <span id="book-rate">${book.rate}</span>
                     <button  onclick="onRangeIncrease(${book.id})">+</button>
                 </div>`
+}
+
+
+
+function openAddSideBar(action, book = null) {
+    document.getElementById("sidebar").style.display = "block";
+    document.getElementById("add-update-form").reset();
+    document.getElementById("add-update-btn").innerText = action === "add" ? "Add Book" : "Update Book";
+    document.getElementById("book-id-input").value = book ? book.id : "";
+    document.getElementById("book-title-input").value = book ? book.title : "";
+    document.getElementById("book-price-input").value = book ? book.price : "";
+    document.getElementById("book-image-input").value = book ? book.image : "";
+    if (book) {
+        document.getElementById("add-update-btn").addEventListener("click", onUpdateBook)
+    }
+    else {
+
+        document.getElementById("add-update-btn").addEventListener("click", onAddBook)
+    }
+    document.getElementById("task-name").innerHTML = action === "add" ? "Add Book" : "Update Book";
+
+}
+
+function onAddBook(event) {
+    event.preventDefault();
+    document.getElementById("sidebar").style.display = "none";
+
+    let book = {
+        id: document.getElementById("book-id-input").value,
+        title: document.getElementById("book-title-input").value,
+        price: parseFloat(document.getElementById("book-price-input").value),
+        image: document.getElementById("book-image-input").value
+    };
+    Gbooks.push(book);
+    localStorage.setItem('books', JSON.stringify(Gbooks));
+    if (CurrentBook >= Gbooks.length - 5) {
+        renderBooks(Gbooks.slice(CurrentBook, CurrentBook + 5));
+    }
+    renderNavigateButtons(Math.ceil(Gbooks.length / 5), Math.floor((CurrentBook + 5) / 5));
+}
+
+
+function onUpdateBook(event) {
+    event.preventDefault();
+
+    document.getElementById("sidebar").style.display = "none";
+    let bookId = parseInt(document.getElementById("book-id-input").value);
+    let book = Gbooks.find(b => b.id === bookId);
+    if (book) {
+        book.title = document.getElementById("book-title-input").value;
+        book.price = parseFloat(document.getElementById("book-price-input").value);
+        book.image = document.getElementById("book-image-input").value;
+
+        localStorage.setItem('books', JSON.stringify(Gbooks));
+
+        renderBooks(Gbooks.slice(CurrentBook, CurrentBook + 5));
+    } else {
+        console.error("Book not found");
+    }
+}
+function closeSidebar() {
+    document.getElementById("sidebar").style.display = "none";
 }
